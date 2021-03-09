6af4c35db79534728369590988ab7c180e3f57ef
Anton Emelyanov <king_menin@gmail.com>
2021-03-10 02:13:52 +0300

    fix running script

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -1,10 +1,24 @@
 #! /bin/bash
 
-total_processes=$1
-weights_path=$2
+total_processes=16
+weights_path="/home/jovyan/runormas/models/xl/v5/20000/mp_rank_00_model_states.pt"
+path="../data/public_test"
+output_dir="../data/public_test_raw"
+part="test"
+
+run () {
+  local idx="$1"
+  python predict.py --num_proc="$idx" \
+    --master_port="600$idx" \
+    --weights_path="$weights_path" \
+    --path="$path" \
+    --output_dir="$output_dir" \
+    --part="$part"
+}
 
 for (( i=0; i < total_processes; i++ ))
 do
-python predict.py --num_proc="$i" --master_port="600$i" --weights_path="$weights_path" &
-
+  run $i &
 done
+
+wait
